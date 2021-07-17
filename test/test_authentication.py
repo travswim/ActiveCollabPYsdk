429b89f01ba9c74cb0af528c7b5a668fb1991a00
Travis Booth <travswim@gmail.com>
2021-07-17 00:17:28 -0700

    First test case completed for connecting and issuing a token

--- test/test_authentication.py
+++ test/test_authentication.py
@@ -1,34 +1,40 @@
-import requests
-import json
-
 import os
 from dotenv import load_dotenv
+import unittest
+from activecollabpysdk.Cloud import Cloud
 
-load_dotenv()
+class Test_Login(unittest.TestCase):
+    def test_login_success(self):
+    
+        load_dotenv()
 
-url = os.getenv('AC_URL')
-my_email = os.getenv('AC_EMAIL')
-my_password = os.getenv(('AC_PASSWORD'))
+        # url = os.getenv('AC_URL')
+        my_email = os.getenv('AC_EMAIL')        # str
+        my_password = os.getenv('AC_PASSWORD')  # str
+        my_ac_url = os.getenv('AC_ACCOUNT_URL') # str
+        
+        my_account_id = os.getenv('AC_ACCOUNT_ID') # int
 
+        if type(my_account_id) == str:
+            my_account_id = int(my_account_id)
+        else:
+            raise ValueError("Empty account ID")
+        my_organization = 'My Organization Inc'
+        my_app = 'My Dummy App'
 
+        if my_email and my_password:
+            my_cloud = Cloud(my_organization, my_app, my_email, my_password)
+        else:
+            raise AttributeError('Password and email are empty')
 
+        self.assertTrue(my_cloud.accounts, "Accounts is empty")
+        self.assert_(my_account_id in my_cloud.accounts)
+        self.assertEquals(my_cloud.accounts[my_account_id]['url'], my_ac_url, 'Wrong URL returned')
+        self.assertEquals(my_cloud.accounts[my_account_id]['class'], 'FeatherApplicationInstance', 'Wrong application type returned')
+        
+        # Testing issue a token
+        my_token = my_cloud.issue_token(my_account_id)
+        # print(my_token)
 
-if (url == None or my_email == None or my_password == None):
-    raise ValueError(f'Cannot be empty value:\nurl = {url}\nmy_email = {my_email}\nmy_password = {my_password}')
-
-data = {"email": my_email, "password": my_password}
-headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-
-try:
-    r = requests.post(url=str(url), json=data, headers=headers)
-    # r = requests.post(url, json=json.dumps(data), headers=headers)
-    r.raise_for_status()  
-except requests.HTTPError as e:
-    raise SystemExit(e)
-
-except requests.exceptions.RequestException as e:
-    raise SystemExit(e)
-
-print(r.status_code, r.reason)
-print(json.dumps(r.json(), indent=4, sort_keys=True))
-print(r.headers)
+if __name__ == '__main__':
+    unittest.main()
