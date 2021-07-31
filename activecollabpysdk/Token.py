98f7c25ea4d2bde6613349317ea366dd3eac0d53
Travis Booth <travswim@gmail.com>
2021-07-30 21:19:01 -0700

    Converted token to dataclass refactored

--- activecollabpysdk/Token.py
+++ activecollabpysdk/Token.py
@@ -1,35 +1,17 @@
+from typing import Any
 import validators
 from Exceptions import InvalidArgumentError
-class Token:
+from dataclasses import dataclass   
 
-    def __init__(self, token: str, url) -> None:
-        self.__token = token
-        if validators.url(url):
-            self.__url = url
-        else:
-            raise InvalidArgumentError("Not a valid URL")
+@dataclass
+class Tokens:
+
+    token: str
+    url: str
+
+    def __post_init__(self):
+        if not validators.url(Any(self.url)):
+            raise InvalidArgumentError(f'{self.url} is not a valid URL')
 
     def __str__(self) -> str:
         return f'Token: {self.token}\n URL: {self.url}'
-
-    def __repr__(self) -> str:
-        return f'Token: {self.token}\n URL: {self.url}'
-
-    @property
-    def token(self):
-        return self.__token
-
-    @token.setter
-    def token(self, value: str):
-        self.__token = value
-
-    @property
-    def url(self):
-        return self.__url
-        
-    @url.setter
-    def url(self, value):
-        if validators.url(value):
-            self.__url = value
-        else:
-            raise InvalidArgumentError("Not a valid URL")
