2c9ceb3eaf6723ac25b55634a3ad948b5e68e6c2
Travis Booth <travswim@gmail.com>
2022-08-30 21:36:11 -0700

    added client tests

--- test/test_client.py
+++ test/test_client.py
@@ -1,8 +1,13 @@
-import imp
+import json
+from io import BufferedReader
 import unittest
+from unittest.mock import patch
 import configparser
 
-from activecollabpysdk.Exceptions import EmptyArgumentError
+# from mock import patch
+from requests import Response
+
+from activecollabpysdk.Exceptions import EmptyArgumentError, InvalidArgumentError
 from activecollabpysdk.token_sdk import Token
 from activecollabpysdk.client import Client
 
@@ -26,13 +31,48 @@ class Test_Client(unittest.TestCase):
         self.my_app = 'My Dummy App'
 
 
-
     def test_prepare_url(self):
-        t = Token('abc', 'https://www.something.com')
-        c = Client(t, 1)
-        c.__prepare_url('info')
-    
+        c = self._test_prepare_client()
 
+        r = c._prepare_url('info')
+        self.assertEqual(r, 'https://www.something.com/api/v1/info')
+
+    def test_prepare_url_fail(self):
+        c = self._test_prepare_client()
+        self.assertRaises(InvalidArgumentError, c._prepare_url, '')
+
+    def test_prepare_files_success(self):
+        c = self._test_prepare_client()
+        file_path = 'README.md'
+        x = c._prepare_files([file_path])
+        self.assertIs(type(list(x.values())[0]), BufferedReader)
+
+    def test_prepare_files_fail_filepath(self):
+        c = self._test_prepare_client()
+        file_path = 'abc.txt'
+        with self.assertRaises(FileNotFoundError):
+            c._prepare_files([file_path])        
+
+    @patch('activecollabpysdk.client.requests.get')
+    def test_info(self, mock_info):
+        mock_info.return_value.status_code = 201
+        mock_info.return_value.json.return_value = 'mock response'
+        c = self._test_prepare_client()
+        actual = c.info()
+        self.assertEqual(actual, 'mock response')
+        mock_info.assert_called_once_with(
+            url='https://www.something.com/api/v1/info',
+            headers={'X-Angie-AuthApiToken': 'abc'}
+        )  
+
+    def _test_prepare_client(self):
+        """
+        Driver function for testing client files
+        """
+        t = Token('abc', 'https://www.something.com')
+        return Client(t, 1)
+
+    
 
 if __name__ == '__main__':
     unittest.main()
