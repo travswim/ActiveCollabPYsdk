55d576b1fe0c8bcb858a959df4f2c02896e4c0a8
Travis Booth <travswim@gmail.com>
2023-07-27 14:08:20 -0700

    Fixing indentation linting

--- activecollabpysdk/Exceptions.py
+++ activecollabpysdk/Exceptions.py
@@ -3,9 +3,15 @@ from requests.exceptions import HTTPError
 
 class EmptyArgumentError(ValueError):
     pass
+
+
 class InvalidArgumentError(ValueError):
     pass
+
+
 class AuthenticationError(ValueError):
     pass
+
+
 class InvalidResponse(HTTPError):
     pass
